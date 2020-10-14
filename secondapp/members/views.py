aea7badb828c2c07fdebe1aadeee81018223c4d0
bres <geobres@gmail.com>
2020-10-14 22:37:18 +0300

    create login and logout

--- secondapp/members/views.py
+++ secondapp/members/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from .forms import RegisterForm
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login,logout
+
+
 
 # Create your views here.
 
@@ -8,7 +11,8 @@ def signup_view(request):
     if request.method == 'POST':
         form=RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user=form.save()
+            login(request,user)
             return redirect('blog:home')    
     else:
         form=RegisterForm()        
@@ -21,8 +25,16 @@ def login_view(request):
         form=AuthenticationForm(data=request.POST)
         if form.is_valid():
             #log user
+            user =form.get_user()
+            login(request,user)
             return redirect('blog:home')
     
     else:
         form=AuthenticationForm()
     return render(request,'members/registration/login.html',{'form':form })
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('blog:home')
---
+++ secondapp/static/css/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    extend: {
+       opacity: {
+      "80": "0.80",
+      "85": "0.85",
+      "90": "0.90",
+      "95": "0.95"
+    }
+    
+    },
+  },
+  variants: {},
+  plugins: []
+}
